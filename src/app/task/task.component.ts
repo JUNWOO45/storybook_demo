766496c176f243883514f8f40fa3edac9666a761
junwoo45 <sinyoorang@gmail.com>
2019-12-17 17:59:17 +0900

    Test Storybook

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,23 +1,32 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-
+import { Task } from './task.model';
 
 @Component({
-  selector: 'app-task',
-  templateUrl: './task.component.html',
-  styleUrls: ['./task.component.css']
+  selector: 'task-item',
+  template: `
+    <div class="list-item">
+      <input type="text" [value]="task.title" readonly="true" />
+      <button>hi</button>
+
+    </div>
+  
+  `
 })
 export class TaskComponent implements OnInit {
-
   title: string;
-  
-
-  @Input() task: any;
+  @Input() task: Task;
   @Output() onPinTask: EventEmitter<any> = new EventEmitter();
-  @Output() onArhiveTask: EventEmitter<any> = new EventEmitter();
+  @Output() onArchiveTask: EventEmitter<any> = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onPin(id) {
+    this.onPinTask.emit(id);
   }
 
+  onArchive(id) {
+    this.onArchiveTask.emit(id);
+  }
 }
